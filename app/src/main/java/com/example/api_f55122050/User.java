3dc4f24b78b2156befb9c32a504bcf9ce65f0032
Rezha Aldino Rizaldi <rezhaldino8@gmail.com>
2023-12-19 00:16:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/api_f55122050/User.java
@@ -0,0 +1,28 @@
+package com.example.api_f55122050;
+
+public class User {
+    private String photo;
+    private String name;
+    private String type;
+
+    public String getPhoto() {
+        return photo;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getType() {
+        return type;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+}
+
